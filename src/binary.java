0c0bfb496202e92922822aa4ef6e2d2ee286e7ef
Shri_Ram <shriram173yt@gmail.com>
2025-02-15 17:11:37 +0530

    Message

---
+++ src/binary.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class binary {
+    public static void main(String[] args) {
+        int array[] = {
+                100, 99, 98, 97, 96, 95, 94, 93, 92, 91,
+                90, 89, 88, 87, 86, 85, 84, 83, 82, 81,
+                80, 79, 78, 77, 76, 75, 74, 73, 72, 71,
+                70, 69, 68, 67, 66, 65, 64, 63, 62, 61,
+                60, 59, 58, 57, 56, 55, 54, 53, 52, 51,
+                50, 49, 48, 47, 46, 45, 44, 43, 42, 41,
+                40, 39, 38, 37, 36, 35, 34, 33, 32, 31,
+                30, 29, 28, 27, 26, 25, 24, 23, 22, 21,
+                20, 19, 18, 17, 16, 15, 14, 13, 12, 11,
+                10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//        int target = 3;
+//        int start=0,mid,end=array.length - 1;
+//        int linearCount = 0, binaryCount =0;
+////        LinearSearch
+//        for(int i = 0 ; i < array.length ; i++){
+//            linearCount++;
+//            if(array[i] == target){
+//                System.out.println(i);
+//                System.out.println("The Linear Search Count is : "+linearCount);
+//            }
+//
+//
+//        }
+
+        //BinarySearch
+//        while (start <= end) {
+//            binaryCount++;
+//            mid = (start + end)/2;
+//            if(array[mid] == target){
+//                System.out.println("The Binary Search count is : "+binaryCount);
+//                System.out.println(mid);
+//                break;
+//            } else if (array[mid] > target) {
+//                start = mid + 1;
+//                end = array.length - 1;
+//            } else if (array[mid] < target) {
+//                end = mid - 1;
+//            }
+//
+//        }
+    }
+}
